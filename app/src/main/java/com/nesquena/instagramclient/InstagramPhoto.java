7b593c167ab81322e788ed7e8581b2e475fba912
Sylvaince Djason Irvinton <djasonirvinton.sylvaince@esih.edu>
2015-07-11 01:15:24 -0400

    InstagramClient Preview
    It's a preview version, please  review./cc @codepathreview @codepath

---
+++ app/src/main/java/com/nesquena/instagramclient/InstagramPhoto.java
@@ -0,0 +1,13 @@
+package com.nesquena.instagramclient;
+
+/**
+ * Created by Ing Djason(Admin) on 09-Jul-15.
+ */
+public class InstagramPhoto {
+    public String username;
+    public String caption;
+    public String imageUrl;
+    public int imageHeigth;
+    public int likesCount;
+
+}
